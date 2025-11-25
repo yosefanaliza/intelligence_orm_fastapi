7c2a87cbd8d40a9755cec8b915b4dba266c31b94
Yosef Hershberg <berioo2000@gmail.com>
2025-11-25 12:22:29 +0200

    Rename route files to follow filename_routes.py pattern

--- app/router.py
+++ app/router.py
@@ -2,10 +2,10 @@
 API v1 Router
 """
 from fastapi import APIRouter
-from app.routes.agents import router as agents_router
-from app.routes.terrorists import router as terrorists_router
-from app.routes.reports import router as reports_router
-from app.routes.sql import router as sql_router
+from app.routes.agents_routes import router as agents_router
+from app.routes.terrorists_routes import router as terrorists_router
+from app.routes.reports_routes import router as reports_router
+from app.routes.sql_routes import router as sql_router
 
 api_router = APIRouter()
 
